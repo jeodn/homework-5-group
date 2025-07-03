fb29843d34253a5cd0c6e0e8d1ec7b61ac1cbb81
josh <joshdrvillas@gmail.com>
2025-07-03 18:37:24 -0400

    LoggedInView and LogoutController

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -100,9 +100,11 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
                 // This creates an anonymous subclass of ActionListener and instantiates it.
                 evt -> {
                     if (evt.getSource().equals(logOut)) {
-                        // TODO: execute the logout use case through the Controller
+                        // TODOnk: execute the logout use case through the Controller
                         // 1. get the state out of the loggedInViewModel. It contains the username.
                         // 2. Execute the logout Controller.
+                        final String username1 = loggedInViewModel.getState().getUsername();
+                        logoutController.execute(username1);
                     }
                 }
         );
@@ -138,6 +140,7 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        // TODOnk: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
